f153344223aca2fead5afb1534b5092515ad58b5
Martin Strobel <marstr@microsoft.com>
2017-10-20 10:54:46 -0700

    Updating this sample to target a profile.
    To do this, and make it easy to just run this sample, I had to update
    Glide to target the `dev` branch. This should be a temporary state, and
    once profiles become mainstream, we should update glide.yaml to target a
    release tag again.
    
    Tareting a profile here both helps how how to use it, and makes our
    samples more resilient to breaking changes in the future.

--- sample.go
+++ sample.go
@@ -12,8 +12,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/arm/network"
-	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
+	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/network/mgmt/network"
+	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/resources/mgmt/resources"
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/adal"
 	"github.com/Azure/go-autorest/autorest/azure"
